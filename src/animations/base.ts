149adb2075ddc5121fc945d9df8ec7da394266e5
Abubukker Chaudhary <abubukker.chaudhary@gmail.com>
2022-12-25 22:19:35 -0500

    changes

--- src/animations/base.ts
+++ src/animations/base.ts
@@ -1,6 +1,10 @@
 export default interface BaseAnimation{
   name: string,
+  // Name of Animation
   creator: string,
+  // Creator of ASCII Animation
   baseState: any,
-  nextState(timestamp: number, state: Object): { nextFrame: string, state: Object} 
+  // Initial starting point, or necessary data required to run the animation or output subsequent frames
+  nextState(timestamp: number, state: Object): { nextFrame: string, state: Object }
+  // Logic that determines the next frame of the animation based on the current state. Timestamp variable can be used to tweak speed at which frames are updated. Output is the next frame, and an updated state to reflect the data for subsequent states
 }
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,6 +3,8 @@ import Animation from "../Animation/Animation";
 import ANIMATIONS from "../../animations/exports";
 import BaseAnimation from "../../animations/base";
 
+import github from "../assets/github-mark.png"
+import coffee from "../assets/bmc-logo.png"
 import "./Home.css"
 
 const Home = () => {
@@ -33,14 +35,44 @@ const Home = () => {
 
   return (
     <div className="home-div">
-      <h1 style={{marginLeft: "20px"}}>--------^</h1>
+      <div className="home-top">
+        <h1 style={{ marginLeft: "20px" }}>--------^</h1>
+        <div style={{ display: "flex" }}>
+          <span className="logo-div">
+            <h4>contribute </h4>
+            <a
+              href="https://github.com/lunarfang416/url-animations"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img className="home-logo" src={github} alt="github"></img>
+            </a>
+          </span>
+          <span className="logo-div">
+            <h4>support </h4>
+            <a
+              href="https://www.buymeacoffee.com/lunarfang416"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img className="home-logo" src={coffee} alt="coffee"></img>
+            </a>
+          </span>
+        </div>
+      </div>
       <div className="animation-div">
-      {Object.keys(ANIMATIONS).map((item, i) => {
-        return <Animation animation={ANIMATIONS[item]} changeAnimation={changeAnimation} key={i} />
-      })}
+        {Object.keys(ANIMATIONS).map((item, i) => {
+          return (
+            <Animation
+              animation={ANIMATIONS[item]}
+              changeAnimation={changeAnimation}
+              key={i}
+            />
+          );
+        })}
       </div>
     </div>
-  )
+  );
 }
 
 export default Home;
